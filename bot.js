285981eba248ea50fba7dad39ddfd37183fab2c9
Kai-Pendleton <kai.pendleton5@gmail.com>
2023-02-01 15:29:00 -0500

    Add ability to duplicate channel with !duplicate command

--- bot.js
+++ bot.js
@@ -8,15 +8,15 @@ client.on('ready', () => {
 	console.log('Logged in as ' + client.user.tag +'!');
 });
 
-client.on('messageCreate', (msg) => {
+client.on('messageCreate', async (msg) => {
 
 	if (msg.channel.name === "kai-bot-test") { //Change this name to your own testing channel name
+		
 		splitCommand = msg.content.split(" ");
 
 		if (splitCommand[0] === '!ping') {
 			msg.reply('Pong!')
 		} else if (splitCommand[0] === "!channel") {
-
 			msg.guild.channels.create({
 				name: splitCommand[1], // This will break if invalid channel name given.s
 				type: 0,
@@ -26,6 +26,22 @@ client.on('messageCreate', (msg) => {
 				}]
 			});
 			msg.channel.send("Channel created!");
+		} else if (splitCommand[0] === "!duplicate") {
+			const duplicatedChannel = await msg.guild.channels.create({
+				name: splitCommand[1], // This will break if invalid channel name given.s
+				type: 0,
+				permissionOverwrites: [{
+					id: msg.guild.id,
+					allow: ["ViewChannel"],
+				}]
+			});
+
+			var messages = await msg.channel.messages.fetch({ limit: 10 });
+			for (var message of messages) {
+				duplicatedChannel.send({ content: message[1].content, embeds: message[1].embeds });
+			}
+
+
 		}
 	}
 });
