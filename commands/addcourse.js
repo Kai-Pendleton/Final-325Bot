aecae8ea3653db08569c7ce023e63530cc2854f9
anicpon1 <122909004+anicpon1@users.noreply.github.com>
2023-02-08 19:57:05 -0500

    adjusted template naming scheme

--- commands/addcourse.js
+++ commands/addcourse.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('addclass')
-		.setDescription('adds class to an existing semester'),
+		.setName('addcourse')
+		.setDescription('adds course to an existing semester'),
 	async execute(interaction) {
 		await interaction.reply({
 			content: 'Pong!',
