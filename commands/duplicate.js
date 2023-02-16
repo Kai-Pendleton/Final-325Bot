28965b2a48dcc9a1acedc34b251ea01201d9ef32
Kai-Pendleton <kai.pendleton5@gmail.com>
2023-02-16 18:20:33 -0500

    Modify duplicate.js to duplicate categories instead of individual text channels

--- commands/duplicate.js
+++ commands/duplicate.js
@@ -6,10 +6,10 @@ module.exports = {
 		.setDescription('Duplicates a text channel')
 		.addChannelOption(option =>
 			option.setName('template')
-			.setDescription('Channel to duplicate (Preceeded by #)'))
+			.setDescription('Template class to duplicate (Looks like a folder)'))
 		.addStringOption(option =>
 			option.setName('destination')
-			.setDescription('Name of new channel (only lowercase, 0-9, and - allowed)')),
+			.setDescription('Name of new class (only lowercase, 0-9, and - allowed)')),
 
 	async execute(interaction, client) {
 		const template = interaction.options.getChannel('template');
@@ -20,21 +20,51 @@ module.exports = {
 			ephemeral: true
 		});
 
-		const duplicatedChannel = await interaction.guild.channels.create({
-			name: destination, // This will break if invalid channel name given.
-			type: 0,
+		// Grab all template channel ids and names
+		templateChannelIds = [];
+		templateChannelNames = [];
+		for (channel of template.children.cache) {
+    		templateChannelNames.push(channel[1].name);
+    		templateChannelIds.push(channel[1].id);	
+		}
+
+		console.log(templateChannelNames);
+		console.log(templateChannelIds);
+
+		// Create new class category
+		const duplicatedCategory = await interaction.guild.channels.create({
+			name: destination,
+			type: 4, // 4 is category
 			permissionOverwrites: [{
 				id: interaction.guild.id,
 				allow: ["ViewChannel"],
 			}]
 		});
+		// Duplicate all original template channels into new category
+		for (var i = 0; i < templateChannelNames.length; i++) {
 
-		var messages = await template.messages.fetch({ limit: 50 });
-		messages = Array.from(messages.values());
+			const duplicatedChannel = await interaction.guild.channels.create({
+				name: templateChannelNames[i], // This will break if invalid channel name given.
+				type: 0, // 0 is text channel
+				permissionOverwrites: [{
+					id: interaction.guild.id,
+					allow: ["ViewChannel"],
+				}]
+			});
+			duplicatedChannel.setParent(duplicatedCategory.id);
 
-		for (var i=messages.length-1; i>=0; i--) {
-			await duplicatedChannel.send({ content: messages[i].content, embeds: messages[i].embeds, files: Array.from(messages[i].attachments.values()) });
+			var templateChannel1 = await interaction.guild.channels.fetch(templateChannelIds[i]);
+			console.log(templateChannel1);
+			var templateMessages = await templateChannel1.messages.fetch({ limit: 50 });
+			console.log(templateMessages);
+			templateMessages = Array.from(templateMessages.values());
+
+			for (var j = templateMessages.length-1; j>=0; j--) {
+				await duplicatedChannel.send({ content: templateMessages[j].content, embeds: templateMessages[j].embeds, files: Array.from(templateMessages[j].attachments.values()) });
+			}
+
 		}
+
 
 	},
 };
